956faf87bb5d16e675f62f76beb4cadb628d9f6e
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2024-05-17 11:45:00 +0900

    fix : block
    Block 수정
    SO 기능을 Block에서 수행

--- BlockBreaker/Assets/Scripts/Block.cs
+++ BlockBreaker/Assets/Scripts/Block.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    [SerializeField] BlockSO blockSO;
+    //[SerializeField] BlockSO blockSO;
+    enum Blokenable
+    {
+        able,
+        nope
+    }
+
+    [SerializeField] Blokenable blokenable; //부숴질수있는가? 기본적으로 부숴질수있게 설정
+    [SerializeField] int blokenCount; //몇번만에 깨질것인지
+    [SerializeField] GameObject item = null; //벽돌 깨졌을때 생성될 아이템
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (blockSO.blokenable != 0) return;
+        if (blokenable != 0) return;
 
-        if(--blockSO.blokenCount <= 0)
+        if(--blokenCount <= 0)
         {
-            Instantiate(blockSO.item);
+            if(item != null) Instantiate(item);
             Destroy(gameObject);
         }
     }
