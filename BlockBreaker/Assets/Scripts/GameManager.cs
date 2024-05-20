d56bd8c8e12e03d3172a3350d1f76963a11e9922
MoonHyang-gi <ansgidrl3@gmail.com>
2024-05-20 17:43:27 +0900

    Feat: Game Logic Complete
    - START 버튼 누르면 게임 시작
    - 생명 3개 다 닳으면 게임 오버
    - 게임 오버에서 RESTART 버튼 누르면 다시 실행

---
+++ BlockBreaker/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public Transform ballSpawn;
+    private int life = 3;
+    public GameObject[] lifeSprites;
+
+    private GameObject currentBall;
+
+    private void Start()
+    {
+        SpawnBall();
+    }
+
+    private void SpawnBall()
+    {
+        //오브젝트, 위치, 회전
+        Instantiate(ballPrefab, ballSpawn.position, Quaternion.identity);
+    }
+
+    public void Health()
+    {
+        life--;
+        UpdateLife();
+
+        if(life <= 0 )
+        {
+            GameOver();
+        }
+        else
+        {
+            //공이 사라지면 1초 뒤에 다시 생성
+            Invoke("SpawnBall", 1f);
+        }
+    }
+
+    private void UpdateLife()
+    {
+        for( int i = 0; i < lifeSprites.Length; i++ )
+        {
+            lifeSprites[i].SetActive(i < life);
+        }
+    }
+
+    private void GameOver()
+    {
+        SceneManager.LoadScene("Moon_EndScene");
+    }
+}
