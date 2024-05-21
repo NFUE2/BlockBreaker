8eaed196ed5dd8726e07c5d352243e11b1d4ba04
MoonHyang-gi <ansgidrl3@gmail.com>
2024-05-21 14:14:36 +0900

    Fix: Ball Collision
    Collision이 포함된 물체에 닿으면 한 방향으로만 튕기고 일정하지 못한 속도를 가진 공
    → 튕겨진 위치의 반대 방향으로 튕기고 일정한 속도를 가지도록 변경

--- BlockBreaker/Assets/Scripts/Ball.cs
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -1,22 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
     private GameManager gameManager;
+    private Rigidbody2D rb;
+
+    private float speed = 10f;
 
     private void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.down * speed;
     }
 
     private void Update()
     {
+
         if (transform.position.y < -6f)
         {
             gameManager.Health();
             Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            float paddleWidth = collision.collider.bounds.size.x;
+            float hitPosition = (transform.position.x - collision.transform.position.x) / paddleWidth;
+
+            Vector2 direction = new Vector2(hitPosition, 1).normalized;
+            rb.velocity = direction * speed;
+        }
+        else
+        {
+            rb.velocity = rb.velocity.normalized * speed;
         }
     }
 }
